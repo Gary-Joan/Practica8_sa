ade0299856eb1493f9e6e9fc8f36fa088fb921da
Gary Ortiz <goritz1490@gmail.com>
2020-09-27 15:48:07 -0600

    configuracion inicial y archivos iniciales

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Practica8_sa
-Contenedores: conceptos, Docker, docker compose
+# Proyecto1_SOPES1
+Repositoria para guardar la informacion sobre los modulos que se van a ejecutar en los servidores del proyecto.
---
+++ app.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask, redirect, url_for, request, render_template, jsonify
+from pymongo import MongoClient
+from json import loads, dump
+from bson import json_util
+
+app = Flask(__name__)
+client = MongoClient(
+    os.environ['DB_PORT_27017_TCP_ADDR'],
+    27017,username='root',password='rootpassword')
+db = client["dbsa"]
+mycol= db["listado"]
+
+@app.route('/')
+def home():
+   return "<h2>API SERVIDOR A</h2>"
+
+
+
+
+@app.route('/items')
+def items():
+   
+    _items = mycol.find()
+    items = [item for item in _items]
+    lista = dumps(items, default=str)
+    return lista
+   
+@app.route('/new', methods=['POST'])
+def new():
+    data = request.get_json()
+    #print(data['autor'],flush=True)
+    item_doc = {
+        "autor": data['autor'],
+        "nota": data['nota']
+    } 
+    x = mycol.insert_one(item_doc)
+    return str(x.inserted_id)
+@app.route('/count')
+def contador():
+    _items = mycol.find()
+    items_count={"cantidad ": _items.count()}
+    return dumps(items_count)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=80, debug=True)
