a141d1f9062014f9047f174243637bd2b11379c9
Gary Ortiz <goritz1490@gmail.com>
2020-09-28 19:24:31 -0600

    dfd

--- mongo/init-db.d/seed.js
+++ mongo/init-db.d/seed.js
@@ -1,5 +1,5 @@
-db.test1.drop();
-db.test1.insert_many([
+db.test.drop();
+db.test.insertMany([
     {"type": "cat", "weight": 25, "color": "orange"},
     {"type": "dog", "weight": 35, "color": "black"},
     {"type": "cat", "weight": 10, "color": "black"}
