14e7e8d83ec4475c922da6c6feb39c2f3506416f
Gary Ortiz <goritz1490@gmail.com>
2020-09-28 19:20:20 -0600

    prueba

--- mongo/init-db.d/seed.js
+++ mongo/init-db.d/seed.js
@@ -1,8 +1,5 @@
-// Since Seeding in Mongo is done in alphabetical order... It's is important to keep
-// file names alphabetically ordered, if multiple files are to be run.
-
-db.test.drop();
-db.test.insert_many([
+db.test1.drop();
+db.test1.insert_many([
     {"type": "cat", "weight": 25, "color": "orange"},
     {"type": "dog", "weight": 35, "color": "black"},
     {"type": "cat", "weight": 10, "color": "black"}
