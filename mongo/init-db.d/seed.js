e8c8c0f272e05b176772ddd0b27d37752848936b
Gary Ortiz <goritz1490@gmail.com>
2020-09-28 19:30:26 -0600

    fsdf

--- mongo/init-db.d/seed.js
+++ mongo/init-db.d/seed.js
@@ -1,6 +1,6 @@
 db.test.drop();
 db.test.insertMany([
     {"type": "cat", "weight": 25, "color": "orange"},
-    {"type": "dog", "weight": 35, "color": "black"},
-    {"type": "cat", "weight": 10, "color": "black"}
+    {"type": "dog1", "weight": 35, "color": "black"},
+    {"type": "wold", "weight": 10, "color": "black"}
 ])
